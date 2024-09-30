82aedfeefe8d642da2012eb7cb032008a81fd8e6
voxvulpis <vulpis.vox@gmail.com>
2024-09-30 19:26:54 -0300

    Example code

---
+++ junit5_tutorial/src/main/java/br/unesp/Vector.java
@@ -0,0 +1,33 @@
+package br.unesp;
+
+public class Vector {
+
+    private Vector(){
+
+    }
+
+    public static boolean equal(int[] a, int[] b){
+        if((a == null) || (b == null)){
+            throw new IllegalArgumentException("null argument");
+        }
+        if(a.length != b.length){
+            return false;
+        }
+        for(int i = 0; i < a.length; i++){
+            if(a[i] != b[i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean size(int a, int b){
+        if(a < 0 || b < 0){
+            throw new IllegalArgumentException("Valores negativos não permitidos");
+        }
+
+        return a==b;
+    }
+
+}
---
+++ junit5_tutorial/src/test/java/br/unesp/VectorTest.java
@@ -0,0 +1,15 @@
+package br.unesp;
+
+import org.junit.jupiter.api.Test;
+
+public class VectorTest {
+    @Test
+    void testEqual() {
+
+    }
+
+    @Test
+    void testSize() {
+
+    }
+}
